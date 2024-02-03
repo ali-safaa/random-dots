c0ae62c7ab7b155e57dd51f87afe43221ee77cd0
ali safaa <alisafaaa33@gmail.com>
2024-02-03 20:51:21 +0300

    new complex project

---
+++ script.js
@@ -0,0 +1,15 @@
+const containerDots = document.querySelector("[data-container-of-dots]");
+
+for (let i = 0; i < 64; i++) {
+     const dot = document.createElement("div");
+     dot.classList.add("dot");
+
+     setInterval(() => {
+          const randomTime = Math.floor(Math.random() * 2) + "s";
+          dot.style.setProperty("--timing", randomTime);
+          const arr = ["#7f7fe6", "#5f5f68", "#d2d2e9", "#e5697a"];
+          const randomValue = arr[Math.floor(Math.random() * arr.length)];
+          dot.style.setProperty("--background-color", randomValue);
+     }, 2000);
+     containerDots.appendChild(dot);
+}
